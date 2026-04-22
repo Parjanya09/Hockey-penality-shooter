0ee6aa2224e6d1a4fcd055dc6c9e8644a818dfdb
Pavithra Nelluri <166512862+PavithraNelluri@users.noreply.github.com>
2026-04-22 16:55:44 +0530

    Update shapes.h

--- shapes.h
+++ shapes.h
@@ -1,28 +1,10 @@
-//
-// Created by kunal on 3/3/18.
-//
 
-#ifndef FOOTBALL_PENALTY_SHOOTER_SHAPES_H
-#define FOOTBALL_PENALTY_SHOOTER_SHAPES_H
+
+#ifndef HOCKEY_PENALTY_SHOOTER_SHAPES_H
+#define HOCKEY_PENALTY_SHOOTER_SHAPES_H
 #include "functionalities.h"
 
-//void drawField() {
-//    glPushAttrib(GL_CURRENT_BIT);
-////    glPushAttrib(GL_TEXTURE);
-//    glBegin(GL_QUADS);
-//    glColor3f(1/255.0, 142/255.0, 14/255.0);
-////    GLuint texture;
-////    texture = LoadGLTexture("./resources/grass.bmp");
-////    glBindTexture(GL_TEXTURE_2D, texture);
-////    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
-////    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
-//    glVertex3f(-10, -10, 0);
-//    glVertex3f(10, -10, 0);
-//    glVertex3f(10, 10, 0);
-//    glVertex3f(-10, 10, 0);
-//    glEnd();
-//    glPopAttrib();
-//}
+
 enum objectType{WALL, R_POLE, L_POLE, U_POLE};
 
 class RealObject{
@@ -81,5 +63,5 @@ extern PoleSurface poles[3];
 extern RealObject allObjects[];
 extern Defender defender;
 
-#endif //FOOTBALL_PENALTY_SHOOTER_SHAPES_H
+#endif //HOCKEY_PENALTY_SHOOTER_SHAPES_H
 
